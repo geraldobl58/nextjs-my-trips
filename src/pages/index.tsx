e72448a570e84b67a669f0663810cef1c79ad8c2
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-28 10:48:11 -0300

    feat: Configuring GraphQL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <LinkWrapper href="/about">
-        <InfoOutline size={32} aria-labe="About" />
+        <InfoOutline size={32} aria-label="About" />
       </LinkWrapper>
       <Map />
     </>
