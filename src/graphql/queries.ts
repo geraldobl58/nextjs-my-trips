fcc5d2f7fb34619dd7b25ea99fda5902c02ed1f9
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-28 12:22:25 -0300

    feat: Dynamically creating N generic pages with getStaticPaths and getStaticProps

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,10 +1,23 @@
 import { gql } from 'graphql-request'
 
 export const GET_PAGES = gql`
-  query getPages {
-    pages {
+  query getPages($first: Int) {
+    pages(first: $first) {
       head
       slug
+      body {
+        html
+      }
+    }
+  }
+`
+
+export const GET_PAGE_BY_SLUG = gql`
+  query getPageBySlug($slug: String!) {
+    page(where: { slug: $slug }) {
+      id
+      slug
+      head
       body {
         html
       }
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,38 @@
+import client from 'graphql/client'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/dist/client/router'
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
+
+export default function Page({ head, body }: PageTemplateProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
+  return <PageTemplate head={head} body={body} />
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return { paths, fallback: true }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if (!page) return { notFound: true }
+
+  return {
+    props: {
+      head: page.head,
+      body: page.body.html
+    }
+  }
+}
